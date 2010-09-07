c4572c2f2e4f36105ab38a32bf2e69c3ce9164d8
malie <m.liedl@gmx.de>
2010-09-07 06:14:03 +0200

    added type parameter

--- derive-functor-1.hs
+++ derive-functor-1.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE DeriveFunctor #-}
 
-data M x = A x
-         | B x (N x)
+data M a x = A x
+           | B x (N a x)
            deriving (Show, Functor)
 
-data N x = D x
-         | E x (M x)
+data N a x = D x
+           | E a x (M a x)
            deriving (Show, Functor)
 
-main = print (fmap f (B 3 (E 4 (B 5 (E 6 (A 7))))))
+main = print (fmap f (B 3 (E "abc" 4 (B 5 (E "def" 6 (A 7))))))
        where f = (*2)
 
